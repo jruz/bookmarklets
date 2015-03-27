9665c9b0c2594a9d8b964a0f774e5b648f9ce43c
Javier Ruz <javierruzg@gmail.com>
2015-03-27 16:35:20 +0100

    wip

--- story-points.js
+++ story-points.js
@@ -1,6 +1,16 @@
 var storyPoints = {
   calculate: function() {
-    alert('hola');
+    var text, result = [];
+    $('.tb-stream-as-block').each(function(i, list){
+      var name    = $(list).find('.js-task-list-title').text();
+      var tags    = $(list).find('a[style="color: rgb(0, 0, 0); background-color: rgb(74, 204, 230);"]');
+      var points  = _.reduce(tags, function(total, node){return total + parseInt(node.text);}, 0);
+      return result.push({name: name, points: points});
+    });
+    result.pop();
+    result.push({name: result[0].name + ' + ' + result[1].name, points: result[0].points + result[1].points});
+    text = _.reduce(result, function(text, count){return text + count.name + ': ' + count.points + '\n';}, '');
+    alert(text);
   }
 };
 
