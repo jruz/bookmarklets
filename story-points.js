ad5d2827f93fb379919b8e97fa1fc539415179ce
Javier Ruz <javierruzg@gmail.com>
2015-06-02 00:00:52 +0200

    tag report draft

--- story-points.js
+++ story-points.js
@@ -8,8 +8,11 @@ var storyPoints = {
     document.head.appendChild(style);
     sheet = style.sheet;
     sheet.insertRule(".story-points { width: 70%; margin: 0 auto; border: none; margin-bottom: 30px; }", 0);
-    sheet.insertRule("th { font-weight: 700 !important; border-bottom: 1px solid #ccc; }", 1);
-    sheet.insertRule("td:last-child { text-align: right; }", 2);
+    sheet.insertRule(".story-points th { font-weight: 700 !important; border-bottom: 1px solid #ccc; }", 1);
+    sheet.insertRule(".story-points td:last-child { text-align: right; }", 2);
+    sheet.insertRule(".story-points__title { font-weight: 700; }", 3);
+    sheet.insertRule(".story-points__select { float: right; }", 4);
+    sheet.insertRule(".story-points__select-wrapper { width: 70%; margin: 0 auto 5px auto; }", 5);
   },
   open_all_lists: function () {
     $('.js-toggle-archived').click();
@@ -58,22 +61,38 @@ var storyPoints = {
       , cells = _.reduce(info, function (prev, count) { return prev + '<tr><td>' + count.name + '</td><td>' + count.points + '</td><tr />'; }, '');
     return header + cells + footer;
   },
+  create_select: function (data) {
+    var i
+      , option
+      , select = document.createElement('select');
+    select.className = 'js-list-tag-select story-points__select';
+    for (i = 0; i < data.length; i++) {
+      option = document.createElement('option');
+      option.text = data[i].name;
+      option.value = i;
+      select.add(option);
+    }
+    return select.outerHTML;
+  },
   init: function () {
     var result = []
       , self = this
       , current_sprint
       , done
       , other
-      , content = [];
+      , content = []
+      , tag_points
+      , lists = $('.js-task-list');
 
-    $('.tb-stream-as-block').each(function (i, list) {
-      var name = $(list).find('.js-task-list-title').text()
-        ,  points = self.get_points(list)
-        ,  tag_points = self.get_tag_points(list);
-      //console.log(tag_points);
-      return result.push({name: name, points: points});
+    result = lists.map(function (i, list) {
+      var title = list.getElementsByClassName('js-task-list-title')[0];
+      if (title) {
+        return {
+          name: title.innerText,
+          points: self.get_points(list)
+        };
+      }
     });
-    result.pop(); //for the last empty list
 
     function getCurrentSprint(data) {
       var arr = [];
@@ -87,18 +106,18 @@ var storyPoints = {
 
     function getDone(data) {
       var regex = /^Done|^Release/;
-      return data.filter(function (item) {
-        if (regex.test(item.name)) {
-          return item;
+      return data.filter(function () {
+        if (regex.test(this.name)) {
+          return this;
         }
       });
     }
 
     function getOther(data) {
       var regex = /^Done|^Release|^To Do|^In Progress/;
-      return data.filter(function (item) {
-        if (!regex.test(item.name)) {
-          return item;
+      return data.filter(function () {
+        if (!regex.test(this.name)) {
+          return this;
         }
       });
     }
@@ -112,7 +131,27 @@ var storyPoints = {
     other = getOther(result);
     content.push(this.create_table('Other', other));
 
-    this.create_dialog(content.join());
+    content.push('<div class="story-points__select-wrapper">');
+    content.push('<span class="story-points__title">Report for:</span>');
+    content.push(this.create_select(result));
+    content.push('</div>');
+
+    tag_points = this.get_tag_points(lists[0]);
+    content.push('<div class="js-list-tag-table">');
+    content.push(this.create_table('Tag', tag_points));
+    content.push('</div>');
+
+    this.create_dialog(content.join(''));
+  },
+  select: function (el) {
+    var self = this;
+    $('.js-dialog').on('change', '.js-list-tag-select', function () {
+      var list_id = $(this).val()
+        , list = $('.js-task-list')[list_id]
+        , tag_points = self.get_tag_points(list)
+        , table = self.create_table('Tag', tag_points);
+      $('.js-list-tag-table').html(table);
+    });
   },
   run: function () {
     var self = this;
@@ -120,7 +159,8 @@ var storyPoints = {
     self.open_all_lists();
     setTimeout(function () {
       self.init();
-    }, 1000 * 1);
+    }, 1000 * .8);
+    self.select();
   }
 };
 storyPoints.run();
